76ec800a7dbf0904453337da12595f700514cab4
KevinAlejandroPazMaldonado <111776525+KevinAlePazM@users.noreply.github.com>
2024-11-25 00:18:12 -0400

    add opo

--- src/database/database.js
+++ src/database/database.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
         logging: console.log,
         dialectOption: {
             ssl: {
-                require: false,
+                require: true,
                 rejectUnauthorized: false,
             }
         }
