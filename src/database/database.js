897e4178a076bcefed52c438e57f5ffc9bab7363
KevinAlejandroPazMaldonado <111776525+KevinAlePazM@users.noreply.github.com>
2024-11-25 00:16:37 -0400

    add sro

--- src/database/database.js
+++ src/database/database.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
         logging: console.log,
         dialectOption: {
             ssl: {
-                require: true,
+                require: false,
                 rejectUnauthorized: false,
             }
         }
