2cc9a407023fd8aed43b9688b42b94ca2431f645
KevinAlejandroPazMaldonado <111776525+KevinAlePazM@users.noreply.github.com>
2024-11-24 23:33:16 -0400

    a

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,12 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT, // or 'mysql' or 'sqlite'
         logging: console.log,
+        dialectOption: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        }
     }
 );
 
