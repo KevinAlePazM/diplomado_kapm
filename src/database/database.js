6f471668f95dcc7196631ec11778f29a7fc30a9b
KevinAlejandroPazMaldonado <111776525+KevinAlePazM@users.noreply.github.com>
2024-11-25 20:20:25 -0400

    add diplomado

--- src/database/database.js
+++ src/database/database.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT, // or 'mysql' or 'sqlite'
         logging: console.log,
-        dialectOption: {
+        dialectOptions: {
             ssl: {
                 require: true,
                 rejectUnauthorized: false,
